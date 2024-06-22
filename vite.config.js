b0a6aea8e79e6012b5df2056efe7bfac05dca701
dminton <dminton@trideum.com>
2024-06-22 18:12:35 -0500

    updates

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,22 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        icons: [
+          {
+            src: "public/dumbell.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        theme_color: "#A9A9A9",
+      },
+      registerType: "autoUpdate",
+    }),
+  ],
   base: "/workout-app/",
 });
