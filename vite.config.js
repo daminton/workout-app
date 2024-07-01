6d247e2304aa00eb80fbc2cf0778da0fb310fd81
dminton <dminton@trideum.com>
2024-07-01 18:45:05 -0500

    test for pwa

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,20 @@ export default defineConfig({
     react(),
     VitePWA({
       manifest: {
+        name: "Workout App",
+        short_name: "WorkoutApp",
+        description: "A progressive web application for managing workouts",
+        start_url: "/workout-app/",
+        display: "standalone", // This ensures the app opens in standalone mode
+        background_color: "#FFFFFF",
+        theme_color: "#A9A9A9",
         icons: [
+          {
+            src: "public/dumbell.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
           {
             src: "public/dumbell.png",
             sizes: "512x512",
@@ -16,10 +29,38 @@ export default defineConfig({
             purpose: "any maskable",
           },
         ],
-        theme_color: "#A9A9A9",
       },
       registerType: "autoUpdate",
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === "image",
+            handler: "CacheFirst",
+            options: {
+              cacheName: "images",
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+              },
+            },
+          },
+          {
+            urlPattern: ({ request }) =>
+              request.destination === "script" ||
+              request.destination === "style",
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "static-resources",
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
+              },
+            },
+          },
+        ],
+      },
     }),
+    ,
   ],
   base: "/workout-app/",
 });
