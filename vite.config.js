0add4ca67d1cc8f57a708c79c330e22075f85551
dminton <dminton@trideum.com>
2024-07-01 18:58:21 -0500

    updates for ios support

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -12,7 +11,7 @@ export default defineConfig({
         short_name: "WorkoutApp",
         description: "A progressive web application for managing workouts",
         start_url: "/workout-app/",
-        display: "standalone", // This ensures the app opens in standalone mode
+        display: "standalone",
         background_color: "#FFFFFF",
         theme_color: "#A9A9A9",
         icons: [
@@ -31,36 +30,7 @@ export default defineConfig({
         ],
       },
       registerType: "autoUpdate",
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: ({ request }) => request.destination === "image",
-            handler: "CacheFirst",
-            options: {
-              cacheName: "images",
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
-              },
-            },
-          },
-          {
-            urlPattern: ({ request }) =>
-              request.destination === "script" ||
-              request.destination === "style",
-            handler: "StaleWhileRevalidate",
-            options: {
-              cacheName: "static-resources",
-              expiration: {
-                maxEntries: 50,
-                maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
-              },
-            },
-          },
-        ],
-      },
     }),
-    ,
   ],
   base: "/workout-app/",
 });
