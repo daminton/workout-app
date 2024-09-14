d1df69941cca2432d61154aaba988dc1f60a4f49
dminton <dminton@trideum.com>
2024-09-14 09:04:07 -0500

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -15,15 +15,608 @@ import { cn } from "./lib/utils";
 import { format } from "date-fns";
 import { Plus } from "lucide-react";
 import { ChevronRightIcon, ChevronLeftIcon } from "lucide-react";
+import { Select } from "./components/ui/select";
+import { SelectTrigger } from "./components/ui/select";
+import { SelectValue } from "./components/ui/select";
+import { SelectContent } from "./components/ui/select";
+import { SelectItem } from "./components/ui/select";
 
 export default function App() {
   const [date, setDate] = useState(new Date());
   const [rowsByDate, setRowsByDate] = useState({});
   const [currentUser, setCurrentUser] = useState("defaultUser");
-  const [isCalendarOpen, setIsCalendarOpen] = useState(false);
+  const [selectedWorkout, setSelectedWorkout] = useState(false);
   const [savedWorkouts, setSavedWorkouts] = useState({});
   const [workoutName, setWorkoutName] = useState("");
   const [currentPage, setCurrentPage] = useState("workout");
+
+  const predefinedWorkouts = [
+    {
+      name: "P90X Back & Biceps",
+      exercises: [
+        {
+          exercise: "Wide Front Pull-Ups",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Lawnmowers",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Twenty-Ones",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "One-Arm Cross-Body Curls",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Switch Grip Pull-Ups",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Elbows-Out Lawnmowers",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Standing Bicep Curls",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "One-Arm Concentration Curls",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Corn Cob Pull-Ups",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Reverse Grip Bent-Over Rows",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Open-Arm Curls",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Static-Arm Curls",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Congdon Locomotives",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Crouching Cohen Curls",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "One-Arm Corkscrew Curls",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Chin-Ups",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Seated Bent-Over Back Flys",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Curl-Up/Hammer Downs",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Hammer Curls",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Max Rep Pull-Ups",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Superman",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "In-Out Hammer Curls",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Strip-Set Curls",
+          sets: "4",
+          setsData: [
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+            { reps: "", weight: "" },
+          ],
+        },
+      ],
+    },
+    {
+      name: "P90X Chest, Shoulders, & Triceps",
+      exercises: [
+        {
+          exercise: "Slow-Motion 3-in-1 Push-Ups",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "In & Out Shoulder Flys",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Station Dips",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Plange Push-Ups",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Pike Presses",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Side Tri-Rises",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Floor Flys",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Scarecrows",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Overhead Triceps Extensions",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Two-Twitch Speed Push-Ups",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Y-Presses",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Lying Triceps Extensions",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Side-to-Side Push-Ups",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Pour Flys",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Side-Leaning Triceps Extensions",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "One-Arm Push-Ups",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Weighted Circles",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Throw the Bomb",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Clap or Plyo Push-Ups",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Slo-Mo Throws",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Front-to-Back Triceps Extensions",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "One-Arm Balance Push-Ups",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Fly-Row-Presses",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Dumbbell Cross-Body Blows",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+      ],
+    },
+    {
+      name: "P90X Legs & Back",
+      exercises: [
+        {
+          exercise: "Balanced Lunges",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Calf-Raise Squats",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Reverse Grip Chin-Ups",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Super Skaters",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Wall Squats",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Wide Front Pull-Ups",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Step Back Lunge",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Alternating Side Lunge",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Closed Grip Overhead Pull-Ups",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Single-Leg Wall Squat",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Deadlift Squats",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Switch Grip Pull-Ups",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Three-Way Lunge",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Sneaky Lunge",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Chair Salutations",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Toe-Roll Iso Lunge",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Groucho Walk",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Calf Raises (Toes Out)",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Calf Raises (Feet Parallel)",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Calf Raises (Toes In)",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Closed Grip Overhand Pull-Ups",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "80/20 Siebers-Speed Squats",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+      ],
+    },
+    {
+      name: "P90X Chest & Back",
+      exercises: [
+        {
+          exercise: "Standard Push-Ups",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Wide Front Pull-Ups",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Military Push-Ups",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Reverse Grip Chin-Ups",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Wide Fly Push-Ups",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Closed Grip Overhand Pull",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Decline Push-Ups",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Heavy Pants",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Diamond Push-Ups",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Lawnmowers",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Dive-Bomber Push-Ups",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Back Flys",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+      ],
+    },
+    {
+      name: "P90X Shoulders and Arms",
+      exercises: [
+        {
+          exercise: "Alt. Shoulder Presses",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "In & Out Bicep Curls",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Two-Arm Triceps Kickback",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Alt. Shoulder Presses",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "In & Out Bicep Curls",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Two-Arm Triceps Kickback",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Deep Swimmer's Presses",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Full Sup. Concentration Curls",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Station Dips",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Deep Swimmer's Presses",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Full Sup. Concentration Curls",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Station Dips",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Upright Rows",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Static Arm Curls",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Flip-Grip Twist Tri Kickbacks",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Upright Rows",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Static Arm Curls",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Flip-Grip Twist Tri Kickbacks",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Seated Two-Angle Flys",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Crouching Cohen Curls",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Lying-Down Tri Extensions",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Seated Two-Angle Flys",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Crouching Cohen Curls",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Lying-Down Tri Extensions",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "In & Out Straight Arm Flys",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Congdon Curls",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Side Tri-Rises",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "In & Out Straight Arm Flys",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Congdon Curls",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+        {
+          exercise: "Side Tri-Rises",
+          sets: "1",
+          setsData: [{ reps: "", weight: "" }],
+        },
+      ],
+    },
+  ];
 
   useEffect(() => {
     const storedData = localStorage.getItem("workoutAppData");
@@ -45,6 +638,24 @@ export default function App() {
     };
     localStorage.setItem("workoutAppData", JSON.stringify(data));
   }, [rowsByDate, savedWorkouts, date]);
+
+  const handleWorkoutSelect = (workoutName) => {
+    setSelectedWorkout(workoutName);
+    const workout = predefinedWorkouts.find((w) => w.name === workoutName);
+    if (workout) {
+      const currentDate = date;
+      setRowsByDate({
+        ...rowsByDate,
+        [currentUser]: {
+          ...rowsByDate[currentUser],
+          [currentDate]: workout.exercises.map((exercise, index) => ({
+            ...exercise,
+            key: `${index + 1}`,
+          })),
+        },
+      });
+    }
+  };
 
   const handlePreviousDate = () => {
     const previousDate = new Date(date);
@@ -232,6 +843,21 @@ export default function App() {
             <Button variant="outline" size="icon" onClick={handleNextDate}>
               <ChevronRightIcon className="h-4 w-4" />
             </Button>
+          </div>
+
+          <div className="w-full px-2 mb-4">
+            <Select onValueChange={handleWorkoutSelect} value={selectedWorkout}>
+              <SelectTrigger>
+                <SelectValue placeholder="Select a workout" />
+              </SelectTrigger>
+              <SelectContent>
+                {predefinedWorkouts.map((workout) => (
+                  <SelectItem key={workout.name} value={workout.name}>
+                    {workout.name}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
           </div>
 
           <div className="w-full h-full flex flex-col items-end">
