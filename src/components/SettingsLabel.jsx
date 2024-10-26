9a1b7ecea8c7adb0a41a3e02bcb1309cd477f2fc
Supercoder24 <superfelix24@gmail.com>
2024-10-25 23:27:50 -0500

    Import, export, and appearance toggles

---
+++ src/components/SettingsLabel.jsx
@@ -0,0 +1,5 @@
+export function SettingsLabel({ label }) {
+  return (
+    <h2 className="w-fit mb-2">{label}</h2>
+  )
+}
--- src/components/SettingsPage.jsx
+++ src/components/SettingsPage.jsx
@@ -1,5 +1,7 @@
 import { useRef, useState } from "react";
 import { ModeToggle } from "./mode-toggle";
+import { Download, Upload } from "lucide-react";
+import SettingsItem from "./SettingsItem";
 
 const SettingsPage = ({
   workoutName,
@@ -20,51 +22,53 @@ const SettingsPage = ({
   };
 
   return (
-    <div className="p-5">
-      <ModeToggle />
-      <div>
-        <h2>Workout Options</h2>
-        <input
-          type="text"
-          placeholder="Workout Name"
-          value={workoutName}
-          onChange={(e) => setWorkoutName(e.target.value)}
-        />
-        <button onClick={() => saveWorkout(workoutName)}>Save Workout</button>
-        <select
-          value={selectedWorkout}
-          onChange={(e) => setSelectedWorkout(e.target.value)}
-        >
-          <option value="">Select Workout</option>
-          {Object.keys(savedWorkouts).map((name) => (
-            <option key={name} value={name}>
-              {name}
-            </option>
-          ))}
-        </select>
-        <button onClick={() => loadWorkout(selectedWorkout)}>
-          Load Workout
-        </button>
-        <button onClick={() => updateWorkout(selectedWorkout)}>
-          Update Workout
-        </button>
-        <button onClick={() => deleteWorkout(selectedWorkout)}>
-          Delete Workout
-        </button>
-      </div>
-      <div className="mt-20">
-        <button onClick={exportWorkoutInformation}>
-          Export Workout History
-        </button>
-      </div>
-      <div className="mt-20">
-        <input
-          type="file"
-          ref={fileInputRef}
-          style={{ display: "none" }}
-          onChange={(e) => importWorkoutInformation(e.target.files[0])}
-        />
-        <button onClick={handleImportClick}>Import Workout History</button>
+    <div className="fixed bottom-[60px] top-0 left-0 right-0 flex flex-row justify-center">
+      <div className="p-5 max-w-lg flex flex-col">
+        <div className="h-full">
+          <h2>Workout Options</h2>
+          <input
+            type="text"
+            placeholder="Workout Name"
+            value={workoutName}
+            onChange={(e) => setWorkoutName(e.target.value)}
+          />
+          <button onClick={() => saveWorkout(workoutName)}>Save Workout</button>
+          <select
+            value={selectedWorkout}
+            onChange={(e) => setSelectedWorkout(e.target.value)}
+          >
+            <option value="">Select Workout</option>
+            {Object.keys(savedWorkouts).map((name) => (
+              <option key={name} value={name}>
+                {name}
+              </option>
+            ))}
+          </select>
+          <button onClick={() => loadWorkout(selectedWorkout)}>
+            Load Workout
+          </button>
+          <button onClick={() => updateWorkout(selectedWorkout)}>
+            Update Workout
+          </button>
+          <button onClick={() => deleteWorkout(selectedWorkout)}>
+            Delete Workout
+          </button>
+        </div>
+        <div className="flex flex-row gap-5">
+          <SettingsItem label={"Import"} onClick={handleImportClick}>
+            <input
+              type="file"
+              ref={fileInputRef}
+              style={{ display: "none" }}
+              onChange={(e) => importWorkoutInformation(e.target.files[0])}
+            />
+            <Upload />
+          </SettingsItem>
+          <SettingsItem label={"Export"} onClick={exportWorkoutInformation}>
+            <Download />
+          </SettingsItem>
+          <ModeToggle />
+        </div>
       </div>
     </div>
   );
